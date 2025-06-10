d9593bed34cc761fc224616a19bde6e2185f9cd7
Omri Peer <omripeer@h-MacBook-Pro-sl-Omri.local>
2025-06-10 14:29:48 +0300

    Fix repository configuration - move JitPack to settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,3 @@ plugins {
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
