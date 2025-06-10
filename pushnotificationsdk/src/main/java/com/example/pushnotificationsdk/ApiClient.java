aeaee82e8fb1423043b50bcb0aaf164912a05d52
Omri Peer <omripeer@h-MacBook-Pro-sl-Omri.local>
2025-06-10 20:07:04 +0300

    Update SDK to use custom domain api.notificationspanel.com

--- pushnotificationsdk/src/main/java/com/example/pushnotificationsdk/ApiClient.java
+++ pushnotificationsdk/src/main/java/com/example/pushnotificationsdk/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://sdk-backend-env-1.eba-rjkps84i.us-east-1.elasticbeanstalk.com"; // Remote server
+    private static final String BASE_URL = "http://api.notificationspanel.com"; // Remote server
     private static Retrofit retrofit;
 
     public static PushApiService getService() {
